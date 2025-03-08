6985b9c776d68c560647242367446bfe885150cd
sajid-edninja <ssk73573@gmail.com>
2025-03-08 09:47:56 +0530

    import router

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,11 @@ import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken";
 
+import contentRouter from "../src/routes/content.route";
+import linkRouter from "../src/routes/link.route";
+import tagRouter from "../src/routes/tag.route";
+import userRouter from "../src/routes/user.route";
+
 async function startMongo() {
   await mongoose.connect(process.env.MONGODB_URI + "neuranaut");
   console.log("DB Connected Successfully.");
@@ -10,6 +15,11 @@ async function startMongo() {
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/contents", contentRouter);
+app.use("/api/v1/links", linkRouter);
+app.use("/api/v1/tags", tagRouter);
 
 app.listen(PORT, async () => {
   console.log(`Server is listening on PORT ${PORT}`);
