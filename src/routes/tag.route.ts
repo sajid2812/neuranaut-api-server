d196cb1458411a02bb8a0687bb5243ff1f817ae9
sajid-edninja <ssk73573@gmail.com>
2025-03-08 21:49:39 +0530

    add tag creation endpoint

--- src/routes/tag.route.ts
+++ src/routes/tag.route.ts
@@ -1,5 +1,28 @@
 import { Router } from "express";
+import { z } from "zod";
+
+import { Tag } from "../schemas/tag.model";
 
 const router = Router();
+
+router.post("/", async (req, res) => {
+  try {
+    const { title } = req.body;
+    const requiredBody = z.object({
+      title: z.string().min(3).max(20),
+    });
+    const { success, error } = requiredBody.safeParse(req.body);
+    if (!success) {
+      res.status(400).json({ message: "Invalid input format", error: error });
+      return;
+    }
+    await Tag.create({
+      title: title,
+    });
+    res.status(200).json({ message: "Tag added successfully" });
+  } catch (e) {
+    res.status(400).json({ message: "Tag creation failed" });
+  }
+});
 
 export default router;
